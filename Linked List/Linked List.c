8f72fa07e96a4bfc11a5222767b7a6120df2c177
Fuad Khan <107122588+Fuad-Khan@users.noreply.github.com>
2022-10-08 18:52:32 +0600

    Add files via upload

---
+++ Linked List/Linked List.c
@@ -0,0 +1,35 @@
+// Fuad Khan
+#include<stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    float Data;
+    struct Node *next;
+};
+
+int main(){
+    struct Node *a = NULL;
+    struct Node *b = NULL;
+    struct Node *c = NULL;
+    
+    a = (struct Node*)malloc(sizeof(struct Node));
+    b = (struct Node*)malloc(sizeof(struct Node));
+    c = (struct Node*)malloc(sizeof(struct Node));
+    
+    a->data = 50;
+    b->data = 60;
+    c->data = 70;
+    
+    a->next = b;
+    b->next = c;
+    c->next = NULL;
+    //Traverse in Linklist
+    
+    while(a != NULL){
+        printf("%d -> ",a->data);
+        a = a->next;
+    }
+    
+    return 0;
+}
